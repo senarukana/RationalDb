f5419228e7020a1864cbf1fc2549d4b6e16aef4c
Lizhe <lizhe.ted@gmail.com>
2013-12-23 23:07:17 +0800

    delete

--- vt/tabletserver/proto/interface.go
+++
@@ -1,9 +0,0 @@
-package proto
-
-type QueryExecutor interface {
-	ExecuteFetch(query string, maxrows int, wantfields bool) (qr *proto.QueryResult, err error)
-	ExecuteStreamFetch(query string) (err error)
-	Fields() (fields []proto.Field)
-	FetchNext() (row []sqltypes.Value, err error)
-	CloseResult()
-}
